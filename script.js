832adb642ddb129e829e1dfef5a3d6999e4cf09e
Wasim Mahamod <95435314+wasimmahamod@users.noreply.github.com>
2024-01-17 13:07:06 +0600

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector("h1");
+
+let count = 0;
+let stop = setInterval(() => {
+  h1.innerHTML = count++;
+  if (count == h1.dataset.number) {
+    clearInterval(stop);
+  }
+}, 1000);
